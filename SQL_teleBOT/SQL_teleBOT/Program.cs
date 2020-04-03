464a30d9c0b2540d547454b7621bfabedc52dda1
rogozhkinegorka <rogozhkinegorka@gmail.com>
2020-04-03 22:43:40 +0300

    Creating a project

---
+++ SQL_teleBOT/SQL_teleBOT/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static SQL_teleBOT.ConnectionWithDB;
+
+namespace SQL_teleBOT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
